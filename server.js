0c937c4e25bde52e11692c45a35baf5e61fa6faf
sudhir.kaushik <sudhir.kaushik@indiamart.com>
2018-01-19 17:24:00 +0530

    Trynaa routa you puta

--- server.js
+++ server.js
@@ -4,15 +4,16 @@ const path = require('path');
 var port=process.env.PORT || 8080;
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static('dist'));
+app.use(express.static(__dirname + '/dist'));
+console.log("\nFilepath is "+app.use(express.static(__dirname + '/dist')));
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/src/index.html'));
 });
-console.log("path is "+__dirname);
+console.log("\n\npath is "+__dirname);
 // Start the app by listening on the default
 // Heroku port
 
 
 app.listen(port);
-console.log("Listening on port "+port);
+console.log("\n\tListening on port "+port);
