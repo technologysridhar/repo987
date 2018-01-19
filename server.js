835d52204c6625ae581dc7c5dc7884199bfa9716
sudhir.kaushik <sudhir.kaushik@indiamart.com>
2018-01-19 17:37:48 +0530

    trynna node routa puta

--- server.js
+++ server.js
@@ -2,13 +2,18 @@ const express = require('express');
 const app = express();
 const path = require('path');
 var port=process.env.PORT || 8000;
+
+var appDir = path.dirname(require.main.filename);
+
 // Run the app by serving the static files
 // in the dist directory
 /*app.use(express.static(path.join(__dirname, "dist"));*/
 
 var DIST_DIR = path.join(__dirname, "dist");
+console.log("\n\npath is "+appDir);
+console.log("\n\n Dist path is "+DIST_DIR);
 
-    app.use(express.static(DIST_DIR));
+app.use(express.static(DIST_DIR));
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/src/index.html'));
