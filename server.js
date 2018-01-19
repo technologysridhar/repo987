08f6e1d9dc6857ede13ce6cabbce95688b9cb5fc
sudhir.kaushik <sudhir.kaushik@indiamart.com>
2018-01-19 16:11:25 +0530

    trynna setinn up routes

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+var port=process.env.PORT || 8080;
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
+console.log("path is "+__dirname);
 // Start the app by listening on the default
 // Heroku port
 
 
-app.listen(process.env.PORT || 8080);
+app.listen(port);
 console.log("Listening on port "+port);
