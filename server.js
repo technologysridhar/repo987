2fcebc9f28b3ff8f89900d3e83b1b785e360aae1
sudhir.kaushik <sudhir.kaushik@indiamart.com>
2018-01-19 17:25:59 +0530

    Trynaa routa you puta

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const path = require('path');
 var port=process.env.PORT || 8080;
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
-console.log("\nFilepath is "+app.use(express.static(__dirname + '/dist')));
+app.use(express.static('./dist'));
+console.log("\nFilepath is "+'/dist');
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/src/index.html'));
